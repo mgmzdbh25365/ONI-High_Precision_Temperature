02ed32bf8e1e67f84eae2c11d9a00400d06e6b7f
mgmzdbh25365 <98370342+mgmzdbh25365@users.noreply.github.com>
2026-01-20 01:56:57 +0800

    v1.0.1.1
    修复 尝试解决因不同运行环境导致的问题。
    Fixed - Attempt to resolve issues caused by different runtime environments.

--- Temperature_Patch.cs
+++ Temperature_Patch.cs
@@ -6,34 +6,62 @@ using HarmonyLib;
 
 namespace Main
 {
-	[HarmonyPatch(typeof(GameUtil) , "GetFormattedTemperature")]
+	[HarmonyPatch(typeof(GameUtil) , nameof(GameUtil.GetFormattedTemperature))]
 	public static class Temperature_Patch
 	{
+		static bool Prepare() => typeof(GameUtil).GetMethod(nameof(GameUtil.GetFormattedTemperature)) != null;
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
 		{
 			List<CodeInstruction> code = instr.ToList();
+			bool found = false;
 			foreach (CodeInstruction codeInstruction in code)
 			{
-				if (codeInstruction.opcode == OpCodes.Ldc_R4 && (float)codeInstruction.operand == 0.1f)
+				if (codeInstruction.opcode == OpCodes.Ldc_R4 && (float)codeInstruction.operand == 0.1f){
 					codeInstruction.operand = float.MaxValue;
-				yield return codeInstruction;
+					found = true;
+					Debug.Log("High Precision Temperature: Successfully patched.");
+				}
 			}
+			if (!found){
+				foreach (CodeInstruction codeInstruction in code)
+				{
+					if ((codeInstruction.opcode == OpCodes.Ldc_R4 || codeInstruction.operand is float) && Math.Abs((float)codeInstruction.operand - 0.1f) < 0.0001f){
+						codeInstruction.operand = float.MaxValue;
+						Debug.Log("High Precision Temperature: Successfully patched in other circumstances.");
+					}
+				}
+			}
+			return code;
 		}
 	}
 
     // since [Game Update] - 674504 
-	[HarmonyPatch(typeof(GameUtil) , "AppendFormattedTemperature")]
+	[HarmonyPatch(typeof(GameUtil) , nameof(GameUtil.AppendFormattedTemperature))]
 	public static class Temperature_Patch_New
 	{
+		static bool Prepare() => typeof(GameUtil).GetMethod(nameof(GameUtil.AppendFormattedTemperature)) != null;
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
 		{
 			List<CodeInstruction> code = instr.ToList();
+			bool found = false;
 			foreach (CodeInstruction codeInstruction in code)
 			{
-				if (codeInstruction.opcode == OpCodes.Ldc_R4 && (float)codeInstruction.operand == 0.1f)
+				if (codeInstruction.opcode == OpCodes.Ldc_R4 && (float)codeInstruction.operand == 0.1f){
 					codeInstruction.operand = float.MaxValue;
-				yield return codeInstruction;
+					found = true;
+					Debug.Log("High Precision Temperature: Successfully patched in the new version.");
+				}
 			}
+			if (!found){
+				foreach (CodeInstruction codeInstruction in code)
+				{
+					if ((codeInstruction.opcode == OpCodes.Ldc_R4 || codeInstruction.operand is float) && Math.Abs((float)codeInstruction.operand - 0.1f) < 0.0001f){
+						codeInstruction.operand = float.MaxValue;
+						Debug.Log("High Precision Temperature: Successfully patched in other circumstances in the new version.");
+					}
+				}
+			}
+			return code;
 		}
 	}
 }
